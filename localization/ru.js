99e6e593b4d26a09279887cd9e8525206ba8f48f
Dunaevsky Maxim <dunmaksim@yandex.ru>
2014-08-17 18:10:24 +0400

    Added russian localization

---
+++ localization/ru.js
@@ -0,0 +1,32 @@
+/**
+ * bs_grid v0.9.2 localization - ENGLISH ***************************************
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
+var rsc_bs_dg = {
+
+    columns: "Столбцы",
+    columns_show_row_numbers: "Номера строк",
+    columns_default: "По умолчанию",
+
+    sorting: "Сортировка",
+    sort_ascending: "<i class='glyphicon glyphicon-chevron-up'></i>&nbsp;&nbsp;",
+    sort_descending: "<i class='glyphicon glyphicon-chevron-down'></i>&nbsp;&nbsp;",
+    sort_none: "<i class='glyphicon glyphicon-minus'></i>&nbsp;&nbsp;",
+    sorting_default: "По умолчанию",
+
+    filters: "Фильтры",
+    filters_apply: "Применить",
+    filters_reset: "Сброс",
+
+    select: "Выбор",
+    select_all_in_page: "Всё на странице",
+    deselect_all_in_page: "Ничего на странице",
+    select_inverse_in_page: "Обратить",
+    deselect_all: "Снять выделение",
+
+    row_index_header: "№",
+    no_records_found: "Ничего не найдено"
+};
