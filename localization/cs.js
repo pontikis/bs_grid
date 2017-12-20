450e1567885fca3cff356aab1deac4afd71ffa31
Vítězslav Dvořák <info@vitexsoftware.cz>
2017-12-20 20:04:14 +0100

    Czech localization added

---
+++ localization/cs.js
@@ -0,0 +1,32 @@
+/**
+ * bs_grid v0.9.2 localization - CZECH ***************************************
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
+var rsc_bs_dg = {
+
+    columns: "Sloupce",
+    columns_show_row_numbers: "Čísla řádek",
+    columns_default: "Výchozí",
+
+    sorting: "Třídění",
+    sort_ascending: "<i class='glyphicon glyphicon-chevron-up'></i>&nbsp;&nbsp;",
+    sort_descending: "<i class='glyphicon glyphicon-chevron-down'></i>&nbsp;&nbsp;",
+    sort_none: "<i class='glyphicon glyphicon-minus'></i>&nbsp;&nbsp;",
+    sorting_default: "Výchozí",
+
+    filters: "Filtery",
+    filters_apply: "Proveď",
+    filters_reset: "Reset",
+
+    select: "Volba",
+    select_all_in_page: "Vše na stránce",
+    deselect_all_in_page: "Nic na stránce",
+    select_inverse_in_page: "Obrátit",
+    deselect_all: "Odznačit vše",
+
+    row_index_header: "#",
+    no_records_found: "Žádné záznamy nenalezeny"
+};
